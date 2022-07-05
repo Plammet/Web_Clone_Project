9a80a23ad319dae627f663585d83b8b475fc346e
Plammet <ohjs2000@gmail.com>
2022-07-05 15:17:45 +0900

    로그인 기능 추가(Back)
    로그인, 비밀번호 암호화, 토큰 생성

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
 
-const bodyParser = require("body-parser");
 const { user, User } = require("./models/User");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(cookieParser());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Admin:admin1234@basis.xoikfns.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
@@ -26,6 +29,29 @@ app.post('/register',  (req, res) => {
     if(err) return res.json({ success: false, err})
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  User.findOne({ email : req.body.email }, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "등록되지 않은 이메일입니다."
+      })
+    }  
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+      
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userID : user._id })
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -22,6 +25,46 @@ const userSchema = mongoose.Schema({
     token: String,
     tokenExp: Number
 })
+
+userSchema.pre('save', function( next ){
+    // 비밀번호를 DB에 넣기 전 암호화
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+
+    }
+    else{
+        next()
+    }  
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return callback(err);
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback){
+    var user = this;
+
+    var token = jwt.sign(user._id.toHexString(), 'token')
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return callback(err)
+        callback(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = {User}
