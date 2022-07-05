189630bcfaeeae611983bb388c3b084bd35a999e
Plammet <ohjs2000@gmail.com>
2022-07-05 12:07:18 +0900

    회원 가입 기능 추가(Back)
    회원가입 : client에게서 유저 정보를 받아와서 DB에 삽입

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require("body-parser");
+const { user, User } = require("./models/User");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://Admin:admin1234@basis.xoikfns.mongodb.net/?retryWrites=true&w=majority')
 .then(() => console.log('MongoDB Connected...'))
@@ -9,6 +15,19 @@ mongoose.connect('mongodb+srv://Admin:admin1234@basis.xoikfns.mongodb.net/?retry
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',  (req, res) => {
+  // 회원가입 정보를 client에서 받아와서 DB에 삽입
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
